e21a498ba75cce152af98c676c762629d1d5b93f
RedlyDee <33196838+RedlyDee@users.noreply.github.com>
2017-12-01 12:46:41 -0500

    Add files via upload

--- bowling.py
+++ bowling.py
@@ -1,45 +1,45 @@
+def get_value(character):
+    if character in "123456789":
+        return int(character)
+    elif character.lower() == 'x' or character == '/':
+        return 10
+    elif character == '-':
+        return 0
+    else:
+        raise ValueError()
+
+
 def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
+    
+    for x in range(len(game)):
+        if game[x] == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+            result += get_value(game[x])
+
+        if frame < 10 and get_value(game[x]) == 10:
+            if game[x] == '/':
+                result += get_value(game[x+1])
+            elif game[x].lower() == "x":
+                result += get_value(game[x+1])
+
+                if game[x+2] == '/':
+                    result += 10 - get_value(game[x+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                    result += get_value(game[x+2])
+
+        last = get_value(game[x])
+
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
-        else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+
+        in_first_half = not in_first_half
+
+        if game[x].lower() == 'x':
             in_first_half = True
             frame += 1
+            
     return result
-
-def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
-        return 0
-    else:
-        raise ValueError()
